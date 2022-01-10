0d2c564fd54021902575e99c8f08e6d0890f72f6
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-10 22:57:26 +0900

    fix dataDir

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { fsmgmt } from "../scriptUtils/fsmgmt";
 import { fsutil } from "bdsx/fsutil";
 import path = require("path");
 
-const DATA_DIR = path.join(fsutil.projectPath, "data");
+const DATA_DIR = path.join(fsutil.projectPath, "scriptData");
 fsmgmt.mkdirRecursiveSync(DATA_DIR, new Set<string>([fsutil.projectPath]));
 
 export function GetDataDir(filepath: string) {
