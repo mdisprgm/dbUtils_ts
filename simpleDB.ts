089aa06803466811c81a93603539970989ea7bcf
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-10 23:16:56 +0900

    update simpleDB

--- simpleDB.ts
+++ simpleDB.ts
@@ -1,11 +1,27 @@
+import { fsmgmt } from "../hslib/modules/fsmgmt";
+import { fsutil } from "bdsx/fsutil";
 import { SipmleJson as Json } from "./simpleJson";
+
+import path = require("path");
 
 export class SimpleDB {
     private fullPath: string;
     data: any;
 
+    static readonly DATA_DIR = path.join(fsutil.projectPath, "datas");
+
     static New(filePath: string, initValue: any = {}) {
         return new SimpleDB(filePath, initValue);
+    }
+
+    static GetDirPath(filepath: string) {
+        const dirhas = new Set<string>([this.DATA_DIR]);
+        fsmgmt.mkdirRecursiveSync(
+            path.join(this.DATA_DIR, path.dirname(filepath)),
+            dirhas
+        );
+
+        return path.join(this.DATA_DIR, filepath);
     }
 
     private constructor(filePath: string, initValue: any = {}) {
@@ -20,3 +36,8 @@ export class SimpleDB {
         this.write(this.data);
     }
 }
+
+fsmgmt.mkdirRecursiveSync(
+    SimpleDB.DATA_DIR,
+    new Set<string>([fsutil.projectPath])
+);
