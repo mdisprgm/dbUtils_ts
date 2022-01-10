0b4298bd78a957b56336235fa65cf8ddab9a6722
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-11 00:10:45 +0900

    fix generic

--- simpleDB.ts
+++ simpleDB.ts
@@ -6,7 +6,7 @@ import path = require("path");
 
 export class SimpleDB<T = any> {
     private fullPath: string;
-    data: any;
+    data: T;
 
     static readonly DATA_DIR = path.join(fsutil.projectPath, "data");
 
