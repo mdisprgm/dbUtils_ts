ac6f33706cb338fbc73fae440f5f3c349c8e16d4
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-01-11 13:11:57 +0900

    become NPM package (but not for npm/@bdsx)

--- simpleDB.ts
+++ simpleDB.ts
@@ -3,15 +3,21 @@ import { fsutil } from "bdsx/fsutil";
 import { SipmleJson as Json } from "./simpleJson";
 
 import path = require("path");
+import fs = require("fs");
+import ini = require("ini");
+
+const fIni = fs.readFileSync("dbUtils/sub_path.ini", "utf8");
+const DATA_PATH = ini.parse(fIni)["path"];
 
 export class SimpleDB<T = any> {
     private fullPath: string;
     data: T;
 
-    static readonly DATA_DIR = path.join(fsutil.projectPath, "data");
+    static readonly DATA_DIR = path.join(fsutil.projectPath, DATA_PATH);
 
     static New<T = any>(filePath: string, initValue: any = {}) {
-        if (["/", "\\"].includes(filePath[filePath.length - 1])) throw new Error("Can't extract filename");
+        if (["/", "\\"].includes(filePath[filePath.length - 1]))
+            throw new Error("Can't extract filename");
         return new SimpleDB<T>(filePath, initValue);
     }
 
